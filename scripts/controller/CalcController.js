460541e813ffed76073f0d10dbb5075d0826f9a5
Ana Beatriz <anabiawalker@gmail.com>
2022-01-11 12:57:01 -0300

    Adicionando execução dos botões

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 //Regras de negócio
 class CalcController {
     constructor(){
+        this._operation = []; //guarda tudo oq foi clicado
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
@@ -21,6 +22,78 @@ class CalcController {
         }, 1000)
     }
 
+    clearAll(){
+        this._operation = []
+    }
+    clearEntry(){
+        this._operation.pop() //exclui o último clicado
+    }
+    addOperation(value){
+        this._operation.push(value)
+        console.log(this._operation)
+    }
+    setError(){
+        this.displayCalc = 'Error'
+    }
+
+    //Execução dos botões
+    execBtn(value){      
+        switch(value){
+            case 'ac':
+                this.clearAll()
+                break;
+            
+            case 'ce':
+                this.clearEntry()
+                break;
+            
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+
+            case 'multiplicacao':
+                
+                break;
+            
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case 'ponto':
+                
+                break;
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+                break;
+            
+            default:
+                this.setError();
+                break;
+        }
+    }
+
     //Trata múltiplos eventos ao mesmo tempo
     addEventListenerAll(element, events, fn){
 
@@ -37,7 +110,11 @@ class CalcController {
         //Percorre a lista e para cada botão adiciona o evento click
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace("btn-",""))
+                //Pega o nome das classes dos botões e substitui o btn- por nada, retornando somente o número do botão
+                let textBtn = btn.className.baseVal.replace("btn-","")
+
+                //Executa a ação do botão
+                this.execBtn(textBtn)
             })
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
 
